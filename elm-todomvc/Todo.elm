d6ca397bcbcbcd0ca06326b1a0f539c597ff8ee7
Thanik Bhongbhibhat <thanik@google.com>
2016-01-10 17:16:51 -0800

    Write out all the View functions

--- elm-todomvc/Todo.elm
+++ elm-todomvc/Todo.elm
@@ -24,17 +24,26 @@ https://github.com/evancz/elm-architecture-tutorial/
 
 -}
 
+import Signal exposing (Address, Signal)
+
 import String
 
---import Html exposing (..)
---import Html.Attributes
---import Html.Events
---import Html.Lazy
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (lazy2, lazy3)
+import Json.Decode
 
 
 -- MODEL
 
-type alias ID = Int
+type alias ID =
+  Int
+
+
+type alias Visibility =
+  String
+
 
 type alias Task =
   { id          : ID
@@ -166,3 +175,252 @@ update action model =
 
     ChangeVisibility visibility ->
       { model | visibility = visibility }
+
+
+-- VIEW
+
+is13 : Int -> Result String ()
+is13 code =
+  if code == 13 then
+    Ok ()
+  else
+    Err "not the right key code"
+
+
+onEnter : Address Action -> Action -> Attribute
+onEnter address action =
+  on "keydown"
+    -- need to really learn to create custom decoder
+    -- `keyCode` grabbing Javascript `event.keyCode`
+    --   from the triggered event
+    (Json.Decode.customDecoder keyCode is13)
+    (\ _ -> Signal.message address action)
+
+
+onInput : Address Action -> (String -> Action) -> Attribute
+onInput address action =
+  on "input"
+    targetValue
+    --(Signal.message address << action)
+    (\ value -> Signal.message address (action value) )
+
+
+taskEntry : Address Action -> String -> Html
+taskEntry address task =
+  header
+    [ id "header" ]
+    [ h1 [] [ text "todos" ]
+    , input
+        [ id "new-todo"
+        , placeholder "What needs to be done?"
+        , autofocus True
+        , value task
+        , name "newTodo"
+        , onInput address UpdateInput
+        , onEnter address Add
+        ]
+        []
+    ]
+
+
+todoItem : Address Action -> Task -> Html
+todoItem address todo =
+  li
+    [ classList
+        [ ("completed", todo.completed)
+        , ("editing", todo.editing)
+        ]
+    ]
+    [ div
+        [ class "view" ]
+        [ input
+            [ class "toggle"
+            , type' "checkbox"
+            , checked todo.completed
+            , onClick address
+                <| Check todo.id
+                <| not todo.completed
+            ]
+            []
+        , label
+            [ onDoubleClick address <| EditTask todo.id True ]
+            [ text todo.description ]
+        , button
+            [ class "destroy"
+            , onClick address <| Delete todo.id
+            ]
+            []
+        ]
+    , input
+        [ class "edit"
+        , value todo.description
+        , name "title"
+        , id ("todo-" ++ toString todo.id)
+        , onInput address <| UpdateTask todo.id
+        , onBlur address <| EditTask todo.id False
+        , onEnter address <| EditTask todo.id False
+        ]
+        []
+    ]
+
+
+taskList : Address Action -> Visibility -> List Task -> Html
+taskList address visibility tasks =
+  let
+
+    isVisible todo =
+      case visibility of
+        "Completed" -> todo.completed
+        "Active"    -> not todo.completed
+        _           -> True
+
+    allCompleted =
+      List.all .completed tasks
+
+    cssVisibility =
+      if List.isEmpty tasks then
+        "hidden"
+      else
+        "visible"
+
+  in
+    section
+      [ id "main"
+      -- hide the list if the list is empty
+      , style [ ("visibility", cssVisibility) ]
+      ]
+      [ input
+          [ id "toggle-all"
+          , type' "checkbox"
+          , name "toggle"
+          , checked allCompleted
+          , onClick address
+              <| CheckAll
+              -- toggle the opposite of `allCompleted`
+              -- make either all completed or all incompleted
+              -- (no mix)
+              <| not allCompleted
+          ]
+          []
+      , label
+          [ for "toggle-all" ]
+          [ text "Mark all as complete" ]
+      , ul
+          [ id "todo-list" ]
+          -- show based on `visibility` value
+          ( List.filter isVisible tasks
+              |> List.map (todoItem address)
+          )
+      ]
+
+
+visibilitySwap : Address Action -> String -> Visibility -> Visibility -> Html
+visibilitySwap address uri target actual =
+  li
+    [ onClick address <| ChangeVisibility target ]
+    [ a
+        [ href uri
+        , classList [( "selected", target == actual )]
+        ]
+        [ text target ]
+    ]
+
+
+-- links to toggle visibility of the list
+visibilityControls : Address Action -> Visibility -> List Task -> Html
+visibilityControls address visibility tasks =
+  let
+
+    numTasksCompleted =
+      List.filter .completed tasks
+        |> List.length
+
+    numTasksLeft =
+      List.length tasks - numTasksCompleted
+
+    unitString =
+      if numTasksLeft == 1 then
+        " item"
+      else
+        " items"
+
+  in
+    footer
+      [ id "footer"
+      -- hide the links if the list is empty
+      -- ? a way to bailout the function early if tasks is empty ?
+      , hidden (List.isEmpty tasks)
+      ]
+      [ span
+          [ id "todo-count" ]
+          [ strong [] [ toString numTasksLeft |> text ]
+          , text (unitString ++ " left")
+          ]
+      , ul
+          [ id "filters" ]
+          [ visibilitySwap address "#/" "All" visibility
+          , text " "
+          , visibilitySwap address "#/active" "Active" visibility
+          , text " "
+          , visibilitySwap address "#/completed" "Completed" visibility
+          ]
+      , button
+          [ id "clear-completed"
+          , class "clear-completed"
+          , hidden (numTasksCompleted == 0)
+          , onClick address DeleteComplete
+          ]
+          [ text ( "Clear completed (" ++ toString numTasksCompleted ++ ")" )]
+      ]
+
+
+infoFooter : Html
+infoFooter =
+  let
+    linkInfo { description, linkUrl, linkText } =
+      p
+        []
+        [ text (description ++ " ")
+        , a [ href linkUrl ] [ text linkText ]
+        ]
+  in
+    footer
+      [ id "info" ]
+      [ p [] [ text "Double-click to edit a todo" ]
+
+      , linkInfo
+          { description = "Originally written by"
+          , linkUrl = "https://github.com/evancz"
+          , linkText = "Evan Czaplicki"
+          }
+
+      , linkInfo
+          { description = "Learned and refactored by "
+          , linkUrl = "https://github.com/bhongy"
+          , linkText = "Thanik Bhongbhibhat"
+          }
+
+      , linkInfo
+          { description = "Part of"
+          , linkUrl = "http://todomvc.com"
+          , linkText = "TodoMVC"
+          }
+
+      ]
+
+
+view : Address Action -> Model -> Html
+view address model =
+  div
+    [ class "todomvc-wrapper"
+    , style
+        [ ("visibility", "hidden") ]
+    ]
+    [ section
+        [ id "todoapp" ]
+        [ lazy2 taskEntry address model.input
+        , lazy3 taskList address model.visibility model.tasks
+        , lazy3 visibilityControls address model.visibility model.tasks
+        ]
+    , infoFooter
+    ]
